c9e8043dff994488f5275edd5fa01c5b6edf48cf
OlekZa <113592194+OlekZa@users.noreply.github.com>
2022-09-23 08:25:41 +0200

    Create some_file.py

---
+++ some_file.py
@@ -0,0 +1,16 @@
+import turtle as tr
+xyz = tr.Turtle()
+
+
+range_c = int(input("write a number of branches of flower"))
+radius = int(input("write a number of branches of flower"))
+
+
+def flower(range_c, radius):
+    for i in range(range_c):
+        tr.circle(radius, extent=30)
+        tr.left(140)
+        tr.circle(radius, extent=30)
+
+
+flower(range_c, radius)
